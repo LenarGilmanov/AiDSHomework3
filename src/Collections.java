7358e0d5be3a04d5e04d95c7c8ec9047957a4066
LenarGilmanov <lagilmanovla@gmail.com>
2024-12-21 17:22:11 +0300

    Созданы реализации методов классов Collections и Arrays

---
+++ src/Collections.java
@@ -0,0 +1,27 @@
+import java.util.Comparator;
+import java.util.List;
+
+public class Collections {
+
+    // 1. static  int binarySearch(List> list, T key) - 2 балла
+    public static <T extends Comparable<? super T>> int binarySearch(List<T> list, T key) {
+        return binarySearch(list, key, Comparator.naturalOrder());
+    }
+
+    // 2. static  int binarySearch(List list, T key, Comparator c) - 2 балла
+    public static <T> int binarySearch(List<T> list, T key, Comparator<? super T> c) {
+        int fromIndex = 0, toIndex = list.size();
+        while (fromIndex < toIndex) {
+            int mid = (fromIndex + toIndex) >>> 1;
+            int cmp = c.compare(list.get(mid), key);
+            if (cmp < 0) {
+                fromIndex = mid + 1;
+            } else if (cmp > 0) {
+                toIndex = mid;
+            } else {
+                return mid;
+            }
+        }
+        return -(fromIndex + 1);
+    }
+}
